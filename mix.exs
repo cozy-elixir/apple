7ce7c2ccbf2d2e50d8abb161f597d49251d52b3a
c4710n <c4710n@users.noreply.github.com>
2024-06-28 14:11:38 +0800

    release: bump v0.2.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Apple.MixProject do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.2.0"
   @description "Utilities for building Apple SDKs."
   @source_url "https://github.com/cozy-elixir/apple"
 
