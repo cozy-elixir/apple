49244cf5e0953a15b3333527b0e03d312f2a7e1b
c4710n <c4710n@users.noreply.github.com>
2024-06-29 21:43:29 +0800

    release: bump v0.3.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Apple.MixProject do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.3.0"
   @description "Utilities for building Apple SDKs."
   @source_url "https://github.com/cozy-elixir/apple"
 
@@ -9,7 +9,7 @@ defmodule Apple.MixProject do
     [
       app: :apple,
       version: @version,
-      elixir: "~> 1.14",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: @description,
