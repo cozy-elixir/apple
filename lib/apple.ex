0a9aaec9d37fa6aa17fc62828ef5be200d85f8d0
c4710n <c4710n@users.noreply.github.com>
2024-09-25 22:26:34 +0800

    docs: add a note

--- lib/apple.ex
+++ lib/apple.ex
@@ -1,5 +1,12 @@
 defmodule Apple do
   @moduledoc """
   Utilities for building Apple SDKs.
+
+  > #### Note {: .info}
+  >
+  > This is not a one-stop, comprehensive SDK. As the description implies, it
+  > provides utilities for building your own minimalistic and focused SDK.
+  >
+  > Read more about this idea at [Ship utilities for building platform SDKs](https://github.com/cozy-elixir/proposals/blob/main/ship-utilities-for-building-platform-sdks.md).
   """
 end
